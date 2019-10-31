705d7442edd14922e06ab27eaa54ca6fc1db2dac
sudarshankumart <57182743+sudarshankumart@users.noreply.github.com>
2019-10-31 10:36:23 +0530

    Update demo6.java

--- demo6.java
+++ demo6.java
@@ -2,7 +2,7 @@ package com.javaproject;
 
 public class demo6{
 
-	public static void main(String[],args){
+	public static void main(String[] args){
 		int[] x= {1,2,3,4};
 		System.out.println(x);
 		System.out.println(x.length);
@@ -11,7 +11,7 @@ public class demo6{
 		System.out.println(x[b]);
 		System.out.println(x[b-1]);
 		System.out.println(x[10]);
-		System.out.println(x(x.length));
+		System.out.println(x[x.length]);
 		System.out.println("end od main");
 	}
 }		     
